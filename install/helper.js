f578a9b9d7e0feac21edd0c163ab1519d29218d8
Zack Bloom <zackbloom@gmail.com>
2015-06-08 14:32:26 -0400

    Add install files

---
+++ install/helper.js
@@ -0,0 +1,34 @@
+(function(){
+
+  var options = INSTALL_OPTIONS;
+
+  var el = Eager.createElement(options.location);
+
+  if (options.title){
+    var title = document.createElement("h2");
+    title.innerHTML = options.title;
+    el.appendChild(title);
+  }
+
+  var container = document.createElement("div");
+  for (var i=0; i < options.items.length; i++){
+    var step = document.createElement("div");
+
+    if (options.items[i].title){
+      var title = document.createElement("h3");
+      title.innerHTML = options.items[i].title;
+      step.appendChild(title);
+    }
+
+    step.innerHTML += options.items[i].content;
+
+    container.appendChild(step);
+  }
+  el.appendChild(container);
+
+  var settings = {};
+  settings.autoplay = options.autoplay;
+
+  container.slick = new Slick(container, settings);
+
+})();

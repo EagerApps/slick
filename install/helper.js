8f42455a4ce55b0e8365b9f92705c3b6e09787af
Zack Bloom <zackbloom@gmail.com>
2015-06-08 14:35:33 -0400

    Add init code

--- install/helper.js
+++ install/helper.js
@@ -1,34 +1,36 @@
 (function(){
 
-  var options = INSTALL_OPTIONS;
+  window.EagerSlick = {
+    init: function(options){
+      var el = Eager.createElement(options.location);
 
-  var el = Eager.createElement(options.location);
+      if (options.title){
+        var title = document.createElement("h2");
+        title.innerHTML = options.title;
+        el.appendChild(title);
+      }
 
-  if (options.title){
-    var title = document.createElement("h2");
-    title.innerHTML = options.title;
-    el.appendChild(title);
-  }
+      var container = document.createElement("div");
+      for (var i=0; i < options.items.length; i++){
+        var step = document.createElement("div");
 
-  var container = document.createElement("div");
-  for (var i=0; i < options.items.length; i++){
-    var step = document.createElement("div");
+        if (options.items[i].title){
+          var title = document.createElement("h3");
+          title.innerHTML = options.items[i].title;
+          step.appendChild(title);
+        }
 
-    if (options.items[i].title){
-      var title = document.createElement("h3");
-      title.innerHTML = options.items[i].title;
-      step.appendChild(title);
+        step.innerHTML += options.items[i].content;
+
+        container.appendChild(step);
+      }
+      el.appendChild(container);
+
+      var settings = {};
+      settings.autoplay = options.autoplay;
+
+      container.slick = new Slick(container, settings);
     }
-
-    step.innerHTML += options.items[i].content;
-
-    container.appendChild(step);
-  }
-  el.appendChild(container);
-
-  var settings = {};
-  settings.autoplay = options.autoplay;
-
-  container.slick = new Slick(container, settings);
+  };
 
 })();
